0ea8be467f8343d4b3faa4a69acd675f388ea9c9
courier3434 <lacourcaitlin@gmail.com>
2020-02-27 02:20:02 +0000

    Movie Array

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -6,20 +6,31 @@ class SurveyController < ApplicationController
 
 
 def filter
+      categories = survey_params
+      movie_options = []
+      categories.shift
       
-      params = survey_params
-      params.category
+      Movie.all.each do |m|
+        categories.each do |category| 
 
-      if params.question == 'your answer'
-        @movies = Movie.findAll(category: params.category)
-
-        render :some_view
+          if m.category.include?('action') && movie_options.length < 30
+            movie_options.push(m)
+          end
+        end
       end
+      @movies = movie_options.uniq
+      
+
+  if params.question == 'your answer'
+        @movies = Movie.findAll(category: params.category)
+        render :some_view
+    end
   end
 
   private
 
   def survey_params
-    params.permit(:name, :category)
+    params[:some_symbol][:categories]
+    # params.require(:some_symbol).permit(:categories)
   end
 end
